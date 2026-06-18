283e28a0c53d2f7bc4daaab8021513d5c19ae360
nigmagrid <nigmagridindonesia@gmail.com>
2026-06-19 00:06:47 +0700

    update

--- config/database.php
+++ config/database.php
@@ -95,8 +95,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'search_path' => 'public',
-            'search_path' => env('DB_SCHEMA', 'kedai_ubi_ungu_fiftycabin'),
+            'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
